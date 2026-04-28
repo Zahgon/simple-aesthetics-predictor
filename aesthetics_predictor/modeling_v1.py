53ccc76b7482df6261b4d78ea5abb5460043061c
piyushpal2004-del <piyush.pal2004@ethara.ai>
2026-04-28 12:18:09 +0530

    Commit 0

--- aesthetics_predictor/modeling_v1.py
+++ aesthetics_predictor/modeling_v1.py
@@ -29,29 +29,7 @@ class AestheticsPredictorV1(CLIPVisionModelWithProjection):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, ImageClassifierOutputWithNoAttention]:
-        return_dict = (
-            return_dict if return_dict is not None else self.config.use_return_dict
-        )
-
-        outputs = super().forward(
-            pixel_values=pixel_values,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-        image_embeds = outputs[0]  # image_embeds
-        image_embeds /= image_embeds.norm(dim=-1, keepdim=True)
-
-        prediction = self.predictor(image_embeds)
-
-        if not return_dict:
-            return (None, prediction, image_embeds)
-
-        return ImageClassifierOutputWithNoAttention(
-            loss=None,
-            logits=prediction,
-            hidden_states=image_embeds,
-        )
+        pass
 
 
 def convert_from_openai_clip(
--- aesthetics_predictor/modeling_v2.py
+++ aesthetics_predictor/modeling_v2.py
@@ -44,34 +44,7 @@ class AestheticsPredictorV2Linear(CLIPVisionModelWithProjection):
         labels: Optional[torch.Tensor] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, ImageClassifierOutputWithNoAttention]:
-        return_dict = (
-            return_dict if return_dict is not None else self.config.use_return_dict
-        )
-
-        outputs = super().forward(
-            pixel_values=pixel_values,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-        image_embeds = outputs[0]  # image_embeds
-        image_embeds /= image_embeds.norm(dim=-1, keepdim=True)
-
-        prediction = self.layers(image_embeds)
-
-        loss = None
-        if labels is not None:
-            loss_fct = nn.MSELoss()
-            loss = loss_fct()
-
-        if not return_dict:
-            return (loss, prediction, image_embeds)
-
-        return ImageClassifierOutputWithNoAttention(
-            loss=loss,
-            logits=prediction,
-            hidden_states=image_embeds,
-        )
+        pass
 
 
 class AestheticsPredictorV2ReLU(AestheticsPredictorV2Linear):
